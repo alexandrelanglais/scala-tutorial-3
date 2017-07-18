4cb2f20b7f0d53286bb66e9a8d5ba27fbff37c02
Alexandre Langlais <alanglaispro@gmail.com>
2017-07-18 14:34:23 +0200

    end of tutorial 3

--- src/main/scala/fr/demandeatonton/classes/ChecksumCalculator.scala
+++ src/main/scala/fr/demandeatonton/classes/ChecksumCalculator.scala
@@ -1,7 +1,5 @@
 package fr.demandeatonton.classes
 
-import scala.collection.mutable.Map
-
 class ChecksumCalculator {
   private var sum = 0;
 
@@ -10,18 +8,13 @@ class ChecksumCalculator {
 }
 
 object ChecksumCalculator {
-  private val cache = Map[String, Int]()
+  def calculate(s: String): Int = {
 
-  def calculate(s: String): Int =
-    if (cache.contains(s))
-      cache(s)
-    else {
-      val acc = new ChecksumCalculator
-      for (c <- s)
-        acc.add(c.toByte)
+    val acc = new ChecksumCalculator
+    for (c <- s)
+      acc.add(c.toByte)
 
-      val cs = acc.checksum()
-      cache += (s -> cs)
-      cs
-    }
+    val cs = acc.checksum()
+    cs
+  }
 }
